8cfdcc58af943cdb509182cf14d735705f3563a9
Debajit Adhikary <debajit@debajit.com>
2017-09-06 14:40:00 -0700

    Fix tests

--- test/plate_slate_web/query/menu_items_test.exs
+++ test/plate_slate_web/query/menu_items_test.exs
@@ -9,74 +9,58 @@ defmodule PlateSlateWeb.Query.MenuItemsTest do
   test "list menu items without filter" do
     query = """
     {
-      menuItems { name}
-    }
-    """
-
-    conn = get build_conn(), "/", query: query # Blank Phoenix connection
-
-    # json_response decodes the JSON
-    assert %{"data" => %{"menuItems" => [item | _]}} = json_response(conn, 200)
-    assert item == %{"name" => "Rueben"}
-  end
-
-  @query """
-  query ($term: String) {
-    menuItems(matching: $term) { name }
-  }
-  """
-
-  test "list menu items with filter nad variables" do
-    variables = %{"term" => "Rue"}
-    conn = get build_conn(), "/", query: @query, variables: variables
-
-    assert %{"data" => %{"menuItems" => [item]}} = json_response(conn, 200)
-    assert item == %{"name" => "Rueben"}
-  end
-
-  test "list menu items without filter with POST" do
-    query = """
-    {
       menuItems { name }
     }
     """
+    conn = get build_conn(), "/", query: query
 
-    conn =
-      build_conn()
-      |> Plug.Conn.put_req_header("content-type", "application/json")
-      |> post("/", %{"query" => query})
-
-    # json_response decodes the JSON
-    assert %{"data" => %{"menuItems" => [item | _]}} = json_response(conn, 200)
-    assert item == %{"name" => "Rueben"}
+    assert %{"data" => %{"menuItems" => [item |_] }} = json_response(conn, 200)
+    assert item == %{"name" => "Bánh mì"}
   end
 
-  test "list menu items with filter" do
-    query = """
-    {
-      menuItems(matching: "Rue") { name }
-    }
-    """
+  @query """
+  query ($filter: MenuItemsFilter) {
+    menuItems(filter:$filter) { name}
+  }
+  """
+  test "list menu items with filter and variables" do
+    variables = %{"filter" => %{"matching" => "Rue"}}
+    conn = get build_conn(), "/", query: @query, variables: variables
 
-    conn =
-      build_conn()
-      |> Plug.Conn.put_req_header("content-type", "application/json")
-      |> post("/", %{"query" => query})
-
-    # json_response decodes the JSON
-    assert %{"data" => %{"menuItems" => [item]}} = json_response(conn, 200)
+    assert %{"data" => %{"menuItems" => [item] }} = json_response(conn, 200)
     assert item == %{"name" => "Rueben"}
   end
 
   test "list menu items with filter + variables + POST" do
-    variables = %{"term" => "Rue"}
-
+    variables = %{"filter" => %{"matching" => "Rue"}}
     conn =
       build_conn()
       |> Plug.Conn.put_req_header("content-type", "application/json")
       |> post("/", %{"query" => @query, "variables" => variables})
 
-    assert %{"data" => %{"menuItems" => [item]}} = json_response(conn, 200)
+    assert %{"data" => %{"menuItems" => [item] }} = json_response(conn, 200)
+    assert item == %{"name" => "Rueben"}
+  end
+
+  test "list menu items without filter with POST" do
+    query = "{ menuItems { name } }"
+    conn =
+      build_conn()
+      |> Plug.Conn.put_req_header("content-type", "application/json")
+      |> post("/", %{"query" => query})
+
+    assert %{"data" => %{"menuItems" => [item |_] }} = json_response(conn, 200)
+    assert item == %{"name" => "Bánh mì"}
+  end
+
+  test "list menu items with filter" do
+    query = "{ menuItems(filter: {matching: \"Rue\"}) { name } }"
+    conn =
+      build_conn()
+      |> Plug.Conn.put_req_header("content-type", "application/json")
+      |> post("/", %{"query" => query})
+
+    assert %{"data" => %{"menuItems" => [item] }} = json_response(conn, 200)
     assert item == %{"name" => "Rueben"}
   end
 end
