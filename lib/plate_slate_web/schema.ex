9aabd117d49dccb77e15098520c81e6f23e54ebd
Debajit Adhikary <debajit@debajit.com>
2017-09-06 09:37:41 -0700

    Add doc

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -12,8 +12,13 @@ defmodule PlateSlateWeb.Schema do
     field :health, :string
   end
 
+  @desc """
+  Tasty thing to eat!
+  """
   object :menu_item do          # Creates struct
     field :id, :id              # Ids in GraphQL get serialized as strings.
+
+    @desc "The name of the item"
     field :name, :string        # Creates a field inside the struct
     field :description, :string
   end
