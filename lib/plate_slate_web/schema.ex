dde9e2f0446b5967115df146b747d8a02ef41fbd
Debajit Adhikary <debajit@debajit.com>
2017-09-06 14:10:23 -0700

    Add decimal scalar type

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -15,8 +15,25 @@ defmodule PlateSlateWeb.Schema do
 
   query do
     field :menu_items, list_of(:menu_item) do
-      arg :matching, :string
+      arg :filter, :menu_items_filter
       resolve &Resolvers.Menu.menu_items/3
+    end
+  end
+
+  # Values sent from the user
+  input_object :menu_items_filter do
+    field :matching, :string
+    field :priced_above, :decimal
+    field :priced_below, :decimal
+  end
+
+  scalar :decimal do
+    parse fn input ->
+      input.value |> Decimal.parse # Ecto relies on the decimal library - used quite widely
+    end
+
+    serialize fn value ->
+      value |> to_string
     end
   end
 
@@ -29,6 +46,6 @@ defmodule PlateSlateWeb.Schema do
     @desc "The name of the item"
     field :name, :string        # Creates a field inside the struct
     field :description, :string
-    field :price, :float
+    field :price, :decimal
   end
 end
