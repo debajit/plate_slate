c0ac20336d5ae4f09ff3940c82044c842afdd62c
Debajit Adhikary <debajit@debajit.com>
2017-09-06 11:18:49 -0700

    Add test for query

---
+++ test/plate_slate_web/query/menu_items_test.exs
@@ -0,0 +1,23 @@
+defmodule PlateSlateWeb.Query.MenuItemsTest do
+  use PlateSlateWeb.ConnCase
+
+  setup do
+    Code.eval_file("priv/repo/seeds.exs")
+    :ok
+  end
+
+  test "list menu items without filter" do
+    query = """
+    {
+      menuItems { name}
+    }
+    """
+
+    payload = %{"query" => query}
+
+    conn = get build_conn(), "/", query: query
+
+    assert %{"data" => %{"menuItems" => [item | _]}} = json_response(conn, 200)
+    assert item == %{"name" => "Rueben"}
+  end
+end
