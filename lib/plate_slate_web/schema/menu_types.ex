0ea3e99341ac1f7e704ce0d51e394387b4331f33
Debajit Adhikary <debajit@debajit.com>
2017-09-06 15:57:17 -0700

    Adding batching test

--- lib/plate_slate_web/schema/menu_types.ex
+++ lib/plate_slate_web/schema/menu_types.ex
@@ -18,7 +18,23 @@ defmodule PlateSlateWeb.Schema.MenuTypes do
     field :description, :string
     field :price, :float
     field :category, :category, resolve: assoc(:category)
+
+    # field :category, :category do
+    #   batch({__MODULE__, :batch_get_categories}, menu_item.category_id, fn batch_results ->
+    #     {:ok, Map.get(batch_results, menu_item.category_id)}
+    #   end)
+    # end
   end
+
+  # def batch_get_categories(_, category_ids) do
+  #   import Ecto.Query
+
+  #   PlateSlate.Menu.Category
+  #   |> where([c], c.id in ^Enum.uniq(category_ids))
+  #   |> Repo.all
+  #   |> Map.new(fn category -> {category.id, category} end)
+  #   |> IO.inspect
+  # end
 
   object :category do
     field :name, :string
